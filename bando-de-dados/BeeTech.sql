db9503068a61baed8801ba2c8fc99f215a3d3fbe
BiaRosaR <125586731+BiaRosaR@users.noreply.github.com>
2023-05-23 13:50:18 -0300

    Atualização Banco de dados e modelagem lógica

--- bando-de-dados/BeeTech.sql
+++ bando-de-dados/BeeTech.sql
@@ -18,7 +18,8 @@ CREATE TABLE Representante (
 
 CREATE TABLE Usuario (
 	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
-    login VARCHAR(45),
+    nome_user VARCHAR(45),
+    email VARCHAR(45),
     senha VARCHAR(45),
     fkEmpresa INT,
 	FOREIGN KEY (fkEmpresa) REFERENCES Empresa (idEmpresa)
@@ -58,26 +59,32 @@ CREATE TABLE Registro (
 	FOREIGN KEY (fkSensor) REFERENCES Sensor (idSensor)
 	);
     
-INSERT INTO empresa VALUES (null,'AgroPiario','39.195.673/0001-80','agro.piario@gmail.com','1234-5678'),
+INSERT INTO Empresa VALUES (null,'AgroPiario','39.195.673/0001-80','agro.piario@gmail.com','1234-5678'),
 						   (null,'Piario','71.607.220/0001-26','piario@gmail.com','1564-2278'),
                            (null,'BeeSmart','80.188.316/0001-98','BEE.SMART@gmail.com','1324-5566'),
                            (null,'Abelhas Brasil','50.688.122/0001-90','abelhas.brasil@gmail.com','1231-5658'),
                            (null,'BeeHoney','84.281.654/0001-20','honey.bee@gmail.com','1874-5476');
                            
-INSERT INTO representante (nomeRepresentante, fkEmpresa) VALUES ("Pedro Augustino", 1),
+SELECT * FROM Empresa;
+                           
+INSERT INTO Representante (nomeRepresentante, fkEmpresa) VALUES ("Pedro Augustino", 1),
 ("Jonas Silva", 1),
 ("Marisa de Almeida", 2),
 ("Roberto Silva", 3),
 ("Carlos Alberto de Santos", 4),
 ("Kassandra Gimenez", 5);
 
-INSERT INTO Usuario (login, senha, fkEmpresa)VALUES
-('Pedro Augustino', '123456', 1),
-("Jonas Silva", '123456',1),
-("Marisa de Almeida", '123456', 2),
-("Roberto Silva", '123456',3),
-("Carlos Alberto de Santos", '123456', 4),
-("Kassandra Gimenez", '123456',5);
+SELECT * FROM Representante;
+
+INSERT INTO Usuario (idUsuario, nome_user, email, senha, fkEmpresa)
+VALUES(NULL, 'Pedro Augustino', 'AugostinoPedro@gmail.com', '123456', 1),
+(NULL, "Jonas Silva", 'joana@gmail.com', '123456',1),
+(NULL, "Marisa de Almeida", 'marisaAlmeida@gmail.com', '123456', 2),
+(NULL, "Roberto Silva", 'bertosilva@gmail.com', '123456',3),
+(NULL, "Carlos Alberto de Santos", 'AlbertoCarlos@gmail.com', '123456', 4),
+(NULL, "Kassandra Gimenez", 'kassandraGimenez@gmail.com', '123456',5);
+
+SELECT * FROM Usuario;
 
 INSERT INTO Endereco VALUES
 	(NULL, 'João Millam', '45', 'Rio Pequeno', 'São Paulo', '08928-833', 1),
@@ -92,12 +99,14 @@ INSERT INTO LocalSensor VALUES(NULL, 'Fazenda da Juta',1),
                               (NULL, 'Fazenda arco-irpis', 3),
                               (NULL, 'Doce Mel', 4),
                               (NULL, 'Happy bee', 5);
+SELECT * FROM LocalSensor;
                               
-INSERT into sensor values (null,'dht11',1),
+INSERT into Sensor values (null,'dht11',1),
 						  (null,'dht11',2),
                           (null,'dht11',3),
                           (null,'dht11',4),
                           (null,'dht11',5);
+SELECT * FROM Sensor;
                           
 INSERT INTO Registro VALUES
 	(NULL, '2023-05-02', 30, 54, 1),
@@ -105,3 +114,5 @@ INSERT INTO Registro VALUES
 	(NULL, '2023-03-22', 33, 46, 3),
 	(NULL, '2023-11-12', 34, 65, 4),
 	(NULL, '2023-04-17', 25, 60, 5);
+    
+SELECT * FROM Registro;
